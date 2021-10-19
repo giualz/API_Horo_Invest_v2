032f6ad3348ff2f79f25dfc71e7b9e885cedff32
giulia.souza <giulia.souza@al.infnet.edu.br>
2021-10-19 10:38:03 -0300

    auth schema added

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,14 +1,17 @@
 const AuthController = require('../controller/AuthController')
+const { loginSchema, registerSchema } = require('../schemas/authSchema')
 
 module.exports = (routes) => {
 
     routes.post(
         '/login',
+        [loginSchema],
         AuthController.login
     );
 
     routes.post(
         '/register',
+        [registerSchema],
         AuthController.register
     );
 
